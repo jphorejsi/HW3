22492fabe26ec4f8e0642818fbbafdf2cbe8c2cf
Ubayd Sharif <shari147@umn.edu>
2023-11-02 20:48:51 -0500

    random search

--- HW3/HW3.cpp
+++ HW3/HW3.cpp
@@ -5,6 +5,10 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <limits>
 
 using namespace std;
 
@@ -74,7 +78,7 @@ Matrix multiply(Matrix m1, Matrix m2) {
 
 Matrix add(const Matrix& matrix1, const Matrix& matrix2) {
 	if (matrix1.rows != matrix2.rows || matrix1.cols != matrix2.cols) {
-		throw std::invalid_argument("Matrix dimensions are not compatible for addition");
+		// throw std::invalid_argument("Matrix dimensions are not compatible for addition");
 	}
 	Matrix result(matrix1.rows, matrix1.cols);
 	for (int i = 0; i < matrix1.rows; i++) {
@@ -91,7 +95,7 @@ Matrix f(Matrix w, Matrix b, Matrix x) {
 
 Matrix minMatrix(Matrix m1, Matrix m2) {
 	if (m1.rows != m2.rows || m1.cols != m2.cols) {
-		throw std::invalid_argument("Matrix dimensions are not compatible for min");
+		// throw std::invalid_argument("Matrix dimensions are not compatible for min");
 	}
 	float sum1 = 0;
 	float sum2 = 0;
@@ -107,6 +111,47 @@ Matrix minMatrix(Matrix m1, Matrix m2) {
 	else return m2;
 }
 
+float sumOfAbsoluteValues(const Matrix& m) {
+    float sum = 0.0f;
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            sum += std::abs(m.elements[i][j]);
+        }
+    }
+    return sum;
+}
+
+// Random search for the input vector that minimizes the output towards zero.
+Matrix randomSearch(Matrix& w1, Matrix& b1, Matrix& w2, Matrix& b2, Matrix& w3, Matrix& b3, int num_attempts) {
+    srand(static_cast<unsigned>(time(0))); // Seed for random number generator
+
+    Matrix best_x(w1.cols, 1);
+    float best_sum = std::numeric_limits<float>::infinity();
+
+    for (int attempt = 0; attempt < num_attempts; ++attempt) {
+        // Generate random input
+        Matrix x(w1.cols, 1);
+        for (int i = 0; i < x.rows; i++) {
+            x.elements[i][0] = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 20.0f - 10.0f; // Random float between -10 and 10
+        }
+
+        // Evaluate the network with the new input
+        Matrix l1 = add(multiply(w1, x), b1);
+        l1.relu();
+        Matrix l2 = add(multiply(w2, l1), b2);
+        l2.relu();
+        Matrix answer = add(multiply(w3, l2), b3);
+
+        // Check if the new input gives a sum of outputs closer to zero
+        float current_sum = sumOfAbsoluteValues(answer);
+        if (current_sum < best_sum) {
+            best_sum = current_sum;
+            best_x = x;
+        }
+    }
+
+    return best_x;
+}
 
 int main()
 {
@@ -133,10 +178,27 @@ int main()
 	l2.relu();
 	Matrix answer = add(multiply(w3, l2), b3);
 
-	for (int i = 0; i < answer.rows; i++) {
-		for (int j = 0; j < answer.cols; j++) {
-			std::cout << answer.elements[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+	int num_attempts = 10000;
+    Matrix best_input = randomSearch(w1, b1, w2, b2, w3, b3, num_attempts);
+
+    std::cout << "Best input vector that minimizes the output towards zero:" << std::endl;
+    for (int i = 0; i < best_input.rows; i++) {
+        std::cout << best_input.elements[i][0] << " ";
+    }
+    std::cout << std::endl;
+
+    // Evaluate the network with the best input found
+    Matrix l1 = add(multiply(w1, best_input), b1);
+    l1.relu();
+    Matrix l2 = add(multiply(w2, l1), b2);
+    l2.relu();
+    Matrix answer = add(multiply(w3, l2), b3);
+
+    std::cout << "Output with the best input vector:" << std::endl;
+    for (int i = 0; i < answer.rows; i++) {
+        std::cout << answer.elements[i][0] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
